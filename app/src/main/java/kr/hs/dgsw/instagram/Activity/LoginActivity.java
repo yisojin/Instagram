bc497480af79c3572fe2722f473152edc334514a
yisojin <tat0402@naver.com>
2018-09-18 09:55:04 +0900

    페이스북 로그인 기능추가, sharedPreference 사용해서 사용자 정보 저장시키기

--- app/src/main/java/kr/hs/dgsw/instagram/Activity/LoginActivity.java
+++ app/src/main/java/kr/hs/dgsw/instagram/Activity/LoginActivity.java
@@ -1,15 +1,30 @@
 package kr.hs.dgsw.instagram.Activity;
 
+import android.content.Context;
 import android.content.Intent;
+import android.content.SharedPreferences;
+import android.content.SharedPreferences.Editor;
+import android.content.pm.PackageInfo;
+import android.content.pm.PackageManager;
+import android.content.pm.Signature;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Base64;
 import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 
-import kr.hs.dgsw.instagram.App;
-import kr.hs.dgsw.instagram.Model.ResponseFormat;
+import com.facebook.CallbackManager;
+import com.facebook.FacebookCallback;
+import com.facebook.FacebookException;
+import com.facebook.login.LoginResult;
+import com.facebook.login.widget.LoginButton;
+
+import java.security.MessageDigest;
+import java.security.NoSuchAlgorithmException;
+
+import kr.hs.dgsw.instagram.Model.ResponseUserFormat;
 import kr.hs.dgsw.instagram.Model.UserModel;
 import kr.hs.dgsw.instagram.Network.Network;
 import kr.hs.dgsw.instagram.R;
@@ -20,17 +35,60 @@ import retrofit.Retrofit;
 
 public class LoginActivity extends AppCompatActivity {
 
+    SharedPreferences sharedPreferences;
+    Editor editor;
+    CallbackManager callbackManager;
+    LoginButton loginButton;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
         getSupportActionBar().hide();
 
-        final App app = new App();
-
         final EditText etAccount = (EditText) findViewById(R.id.etAccount);
         final EditText etPassword = (EditText) findViewById(R.id.etPassword);
         Button btnLogin = (Button) findViewById(R.id.btnLogin);
+
+        sharedPreferences = getSharedPreferences("loginSetting", 0);
+        editor = sharedPreferences.edit();
+
+        try {
+            PackageInfo info = getPackageManager().getPackageInfo(
+                    "kr.hs.dgsw.instagram",
+                    PackageManager.GET_SIGNATURES);
+            for (Signature signature : info.signatures) {
+                MessageDigest md = MessageDigest.getInstance("SHA");
+                md.update(signature.toByteArray());
+                Log.d("KeyHash:", Base64.encodeToString(md.digest(), Base64.DEFAULT));
+            }
+        } catch (PackageManager.NameNotFoundException e) {
+
+        } catch (NoSuchAlgorithmException e) {
+
+        }
+
+        callbackManager = CallbackManager.Factory.create();
+
+        loginButton = (LoginButton) findViewById(R.id.login_button);
+        loginButton.registerCallback(callbackManager, new FacebookCallback<LoginResult>() {
+            @Override
+            public void onSuccess(LoginResult loginResult) {
+                Log.e("fbLogin", loginResult.toString());
+                Intent intent = new Intent(LoginActivity.this, MainActivity.class);
+                startActivity(intent);
+            }
+
+            @Override
+            public void onCancel() {
+                Log.e("fbLogin", "cancel login");
+            }
+
+            @Override
+            public void onError(FacebookException error) {
+                Log.e("fbLogin", error.toString());
+            }
+        });
 
         btnLogin.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -41,14 +99,20 @@ public class LoginActivity extends AppCompatActivity {
 
                 final Network network = Network.retrofit.create(Network.class);
 
-                Call<ResponseFormat> call = network.login(account, password);
-                call.enqueue(new Callback<ResponseFormat>() {
+                Call<ResponseUserFormat> call = network.login(account, password);
+                call.enqueue(new Callback<ResponseUserFormat>() {
 
                     @Override
-                    public void onResponse(Response<ResponseFormat> response, Retrofit retrofit) {
+                    public void onResponse(Response<ResponseUserFormat> response, Retrofit retrofit) {
                         Log.e("result", response.body().toString());
+                        UserModel user = response.body().getData();
+                        if (response.body() != null) {
 
-                        if(response.body() != null){
+                            editor.putString("id", user.getAccount());
+                            editor.putString("password", user.getPassword());
+                            editor.putString("name",user.getName());
+                            editor.commit();
+
                             Intent intent = new Intent(LoginActivity.this, MainActivity.class);
                             startActivity(intent);
                         }
@@ -62,5 +126,11 @@ public class LoginActivity extends AppCompatActivity {
             }
         });
 
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        callbackManager.onActivityResult(requestCode, resultCode, data);
+        super.onActivityResult(requestCode, resultCode, data);
     }
 }
