a0dd32285e7e7212d64696a7253eb87b521f2de5
yisojin <tat0402@naver.com>
2018-09-18 09:55:56 +0900

    기본 ui 변경 (리스트 아이템색상)

--- app/src/main/java/kr/hs/dgsw/instagram/Activity/BoardListActivity.java
+++ app/src/main/java/kr/hs/dgsw/instagram/Activity/BoardListActivity.java
@@ -1,10 +1,16 @@
 package kr.hs.dgsw.instagram.Activity;
 
 import android.content.Intent;
+import android.graphics.Color;
+import android.support.annotation.NonNull;
+import android.support.annotation.Nullable;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
+import android.view.GestureDetector;
+import android.view.MotionEvent;
 import android.view.View;
+import android.view.ViewGroup;
 import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
 import android.widget.CheckBox;
@@ -28,6 +34,7 @@ public class BoardListActivity extends AppCompatActivity {
     ImageButton btn1, btn2, btn3, btn4, btn5;
     Network request;
     ArrayAdapter<String> adapter;
+    ArrayList<BoardModel> listBoard;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -36,6 +43,7 @@ public class BoardListActivity extends AppCompatActivity {
         getSupportActionBar().hide();
 
         final ListView listView = (ListView) findViewById(R.id.lvBoard);
+
 
         btn1 = findViewById(R.id.ibtnHome);
         btn2 = findViewById(R.id.ibtnSearch);
@@ -50,19 +58,32 @@ public class BoardListActivity extends AppCompatActivity {
 
             @Override
             public void onResponse(Response<ResponseListFormat> response, Retrofit retrofit) {
+                Log.e("response", response.body().getData().toString());
                 ArrayList<String> list = new ArrayList<String>();
+                ArrayList<String> listContent = new ArrayList<String>();
 
-                ArrayList<BoardModel> listBoard = new ArrayList<BoardModel>();
+                listBoard = new ArrayList<BoardModel>();
                 for (BoardModel board : response.body().getData()) {
                     listBoard.add(board);
                     list.add(board.getTitle());
                 }
-                adapter = new ArrayAdapter<String>(getApplicationContext(), R.layout.list_item, R.id.text1, list);
+                adapter = new ArrayAdapter<String>(getApplicationContext(), R.layout.list_item, R.id.text1, list) {
+                    @NonNull
+                    @Override
+                    public View getView(int position, @Nullable View convertView, @NonNull ViewGroup parent) {
+                        View view = super.getView(position, convertView, parent);
+                        view.setBackgroundColor(Color.parseColor("#ffffff"));
+                        return view;
+                    }
+                };
+
                 listView.setAdapter(adapter);
+
             }
 
             @Override
             public void onFailure(Throwable t) {
+                Log.e("response", t.getMessage());
 
             }
         });
@@ -70,11 +91,20 @@ public class BoardListActivity extends AppCompatActivity {
         listView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
             @Override
             public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
-                Log.e("select",parent.toString());
+
+                Object obj = listView.getItemAtPosition(position);
+
+                for (BoardModel board : listBoard) {
+                    if (board.getTitle().equals(obj)) {
+                        Log.e("obj selected", board.toString());
+                        Intent intent = new Intent(BoardListActivity.this, ViewBoardActivity.class);
+                        intent.putExtra("id", board.getId());
+                        startActivityForResult(intent, 1);
+
+                    }
+                }
             }
         });
-
-
 
         btn1.setOnClickListener(new View.OnClickListener() {
             @Override
