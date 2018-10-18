c3b1ed6e88fbcdda70111cd70554e72d71e3081d
yisojin <tat0402@naver.com>
2018-10-18 22:24:48 +0900

    image 업로드를 위한 과정, 서버에 업로드하는부분

--- app/src/main/java/kr/hs/dgsw/instagram/Activity/BoardAddActivity.java
+++ app/src/main/java/kr/hs/dgsw/instagram/Activity/BoardAddActivity.java
@@ -1,7 +1,9 @@
 package kr.hs.dgsw.instagram.Activity;
 
+import android.Manifest;
 import android.content.DialogInterface;
 import android.content.Intent;
+import android.content.pm.PackageManager;
 import android.database.Cursor;
 import android.graphics.Bitmap;
 import android.graphics.BitmapFactory;
@@ -10,6 +12,10 @@ import android.media.ExifInterface;
 import android.net.Uri;
 import android.os.Environment;
 import android.provider.MediaStore;
+import android.support.annotation.NonNull;
+import android.support.design.widget.Snackbar;
+import android.support.v4.app.ActivityCompat;
+import android.support.v4.content.ContextCompat;
 import android.support.v4.content.FileProvider;
 import android.support.v7.app.AlertDialog;
 import android.support.v7.app.AppCompatActivity;
@@ -22,8 +28,11 @@ import android.widget.EditText;
 import android.widget.ImageButton;
 import android.widget.ImageView;
 
+import java.io.BufferedOutputStream;
 import java.io.File;
+import java.io.FileOutputStream;
 import java.io.IOException;
+import java.io.InputStream;
 import java.text.SimpleDateFormat;
 import java.util.Date;
 
@@ -31,29 +40,87 @@ import kr.hs.dgsw.instagram.Model.BoardModel;
 import kr.hs.dgsw.instagram.Model.ResponseFormat;
 import kr.hs.dgsw.instagram.Network.Network;
 import kr.hs.dgsw.instagram.R;
+import okhttp3.MediaType;
+import okhttp3.MultipartBody;
+import okhttp3.RequestBody;
 import retrofit.Call;
 import retrofit.Callback;
 import retrofit.Response;
 import retrofit.Retrofit;
 
-public class BoardAddActivity extends AppCompatActivity {
+public class BoardAddActivity extends AppCompatActivity implements ActivityCompat.OnRequestPermissionsResultCallback {
 
     ImageButton btn1, btn2, btn3, btn4, btn5;
     Button btnSend;
     EditText texttitle, textContent, textWriter;
-    private final int CAMERA_CODE = 1111;
-    private final int GALLERY_CODE = 1112;
-    private Uri photoUri;
-    private String currentPhotoPath;//실제 사진 파일 경로
-    String mImageCaptureName;//이미지 이름
     ImageView ivImage;
+    private final int GET_GALLERY_IMAGE = 200;
+    private View mLayout;
+    private static final int PERMISSIONS_REQUEST_CODE = 100;
+    String[] REQUIRED_PERMISSIONS = {Manifest.permission.WRITE_EXTERNAL_STORAGE};
+    private Uri mImageCaptureUri;
+    private String absolutePath;
+    Network network = Network.retrofit.create(Network.class);
 
+
+    @Override
+    public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults) {
+        if (requestCode == PERMISSIONS_REQUEST_CODE && grantResults.length == REQUIRED_PERMISSIONS.length) {
+
+
+            boolean check_result = true;
+
+
+            for (int result : grantResults) {
+                if (result != PackageManager.PERMISSION_GRANTED) {
+                    check_result = false;
+                    break;
+                }
+            }
+
+
+            if (check_result) {
+                ;
+            } else {
+
+                if (ActivityCompat.shouldShowRequestPermissionRationale(this, REQUIRED_PERMISSIONS[0])) {
+
+
+                    Snackbar.make(mLayout, "퍼미션이 거부되었습니다. 앱을 다시 실행하여 퍼미션을 허용해주세요. ",
+                            Snackbar.LENGTH_INDEFINITE).setAction("확인", new View.OnClickListener() {
+
+                        @Override
+                        public void onClick(View view) {
+
+                            finish();
+                        }
+                    }).show();
+
+                } else {
+
+
+                    Snackbar.make(mLayout, "퍼미션이 거부되었습니다. 설정(앱 정보)에서 퍼미션을 허용해야 합니다. ",
+                            Snackbar.LENGTH_INDEFINITE).setAction("확인", new View.OnClickListener() {
+
+                        @Override
+                        public void onClick(View view) {
+
+                            finish();
+                        }
+                    }).show();
+                }
+            }
+
+        }
+    }
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_board_add);
         getSupportActionBar().hide();
+
+        mLayout = findViewById(R.id.layout_main);
 
         btn1 = findViewById(R.id.ibtnHome);
         btn2 = findViewById(R.id.ibtnSearch);
@@ -68,37 +135,6 @@ public class BoardAddActivity extends AppCompatActivity {
         textContent = (EditText) findViewById(R.id.etContent);
 
         ivImage = (ImageView) findViewById(R.id.ivImage);
-
-        btnSend.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                String writer = textWriter.getText().toString();
-                String content = textContent.getText().toString();
-                String title = texttitle.getText().toString();
-
-                BoardModel bm = new BoardModel();
-                bm.setTitle(title);
-                bm.setContent(content);
-                bm.setWriter(writer);
-
-                Network network = Network.retrofit.create(Network.class);
-                Call<ResponseFormat> call = network.post(bm);
-                call.enqueue(new Callback<ResponseFormat>() {
-                    @Override
-                    public void onResponse(Response<ResponseFormat> response, Retrofit retrofit) {
-                        Log.e("result", response.body().toString());
-
-                        Intent intent = new Intent(BoardAddActivity.this, BoardListActivity.class);
-                        startActivity(intent);
-                    }
-
-                    @Override
-                    public void onFailure(Throwable t) {
-                        Log.e("error", t.getMessage());
-                    }
-                });
-            }
-        });
 
         btn1.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -115,14 +151,18 @@ public class BoardAddActivity extends AppCompatActivity {
                 DialogInterface.OnClickListener cameraListener = new DialogInterface.OnClickListener() {
                     @Override
                     public void onClick(DialogInterface dialog, int which) {
-                        SelectPhoto();
+
                     }
                 };
 
                 DialogInterface.OnClickListener albumListener = new DialogInterface.OnClickListener() {
                     @Override
                     public void onClick(DialogInterface dialog, int which) {
-                        selectGallery();
+                        Intent intent = new Intent(Intent.ACTION_PICK);
+                        intent.setData(android.provider.MediaStore.Images.Media.EXTERNAL_CONTENT_URI);
+                        intent.setType("image/*");
+                        startActivityForResult(intent, GET_GALLERY_IMAGE);
+
                     }
                 };
 
@@ -133,7 +173,7 @@ public class BoardAddActivity extends AppCompatActivity {
                     }
                 };
 
-                new AlertDialog.Builder(new ContextThemeWrapper(BoardAddActivity.this, R.style.myDialog)).setTitle("Select Upload Image").setPositiveButton("camera", cameraListener).setNeutralButton("album",albumListener).setNegativeButton("cancel", cancelListener).show();
+                new AlertDialog.Builder(new ContextThemeWrapper(BoardAddActivity.this, R.style.myDialog)).setTitle("Select Upload Image").setPositiveButton("camera", cameraListener).setNeutralButton("album", albumListener).setNegativeButton("cancel", cancelListener).show();
 
             }
         });
@@ -156,144 +196,149 @@ public class BoardAddActivity extends AppCompatActivity {
 
             }
         });
-    }
 
-    private void SelectPhoto() {
-        String state = Environment.getExternalStorageState();
-        if (Environment.MEDIA_MOUNTED.equals(state)) {
-            Intent intent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
-            if (intent.resolveActivity(getPackageManager()) != null) {
-                File photoFile = null;
-                try {
-                    photoFile = createImageFile();
-                } catch (IOException ex) {
+        int writeExternalStoragePermission = ContextCompat.checkSelfPermission(this, Manifest.permission.WRITE_EXTERNAL_STORAGE);
+        if (writeExternalStoragePermission != PackageManager.PERMISSION_GRANTED) {
 
-                }
-                if (photoFile != null) {
-                    photoUri = FileProvider.getUriForFile(this, getPackageName(), photoFile);
-                    intent.putExtra(MediaStore.EXTRA_OUTPUT, photoUri);
-                    startActivityForResult(intent, CAMERA_CODE);
-                }
+            if (ActivityCompat.shouldShowRequestPermissionRationale(this, REQUIRED_PERMISSIONS[0])) {
+
+
+                Snackbar.make(mLayout, "이 앱을 실행하려면 카메라와 외부 저장소 접근 권한이 필요합니다.",
+                        Snackbar.LENGTH_INDEFINITE).setAction("확인", new View.OnClickListener() {
+
+                    @Override
+                    public void onClick(View view) {
+
+                        ActivityCompat.requestPermissions(BoardAddActivity.this, REQUIRED_PERMISSIONS,
+                                PERMISSIONS_REQUEST_CODE);
+                    }
+                }).show();
+
+
+            } else {
+
+                ActivityCompat.requestPermissions(this, REQUIRED_PERMISSIONS,
+                        PERMISSIONS_REQUEST_CODE);
             }
 
         }
     }
 
-    private void selectGallery() {
-
-        Intent intent = new Intent(Intent.ACTION_PICK);
-        intent.setData(android.provider.MediaStore.Images.Media.EXTERNAL_CONTENT_URI);
-        intent.setType("image/*");
-        startActivityForResult(intent, GALLERY_CODE);
-    }
-
-    private File createImageFile() throws IOException {
-        File dir = new File(Environment.getExternalStorageDirectory() + "/path/");
-        if (!dir.exists()) {
-            dir.mkdirs();
-        }
-        String timeStamp = new SimpleDateFormat("yyyyMMdd_HHmmss").format(new Date());
-        mImageCaptureName = timeStamp + ".png";
-
-        File storageDir = new File(Environment.getExternalStorageDirectory().getAbsoluteFile() + "/path/"
-
-                + mImageCaptureName);
-        currentPhotoPath = storageDir.getAbsolutePath();
-
-        return storageDir;
-
-    }
-
-    private void getPictureForPhoto() {
-        Bitmap bitmap = BitmapFactory.decodeFile(currentPhotoPath);
-        ExifInterface exif = null;
-        try {
-            exif = new ExifInterface(currentPhotoPath);
-        } catch (IOException e) {
-            e.printStackTrace();
-        }
-        int exifOrientation;
-        int exifDegree;
-
-        if (exif != null) {
-            exifOrientation = exif.getAttributeInt(ExifInterface.TAG_ORIENTATION, ExifInterface.ORIENTATION_NORMAL);
-            exifDegree = exifOrientationToDegrees(exifOrientation);
-        } else {
-            exifDegree = 0;
-        }
-        ivImage.setImageBitmap(rotate(bitmap, exifDegree));//이미지 뷰에 비트맵 넣기
-    }
-
-    private Bitmap rotate(Bitmap src, float degree) {
-
-// Matrix 객체 생성
-        Matrix matrix = new Matrix();
-// 회전 각도 셋팅
-        matrix.postRotate(degree);
-// 이미지와 Matrix 를 셋팅해서 Bitmap 객체 생성
-        return Bitmap.createBitmap(src, 0, 0, src.getWidth(),
-                src.getHeight(), matrix, true);
-    }
-
-    private int exifOrientationToDegrees(int exifOrientation) {
-        if (exifOrientation == ExifInterface.ORIENTATION_ROTATE_90) {
-            return 90;
-        } else if (exifOrientation == ExifInterface.ORIENTATION_ROTATE_180) {
-            return 180;
-        } else if (exifOrientation == ExifInterface.ORIENTATION_ROTATE_270) {
-            return 270;
-        }
-        return 0;
-    }
 
     @Override
     protected void onActivityResult(int requestCode, int resultCode, Intent data) {
-        super.onActivityResult(requestCode, resultCode, data);
-
-        if (resultCode == RESULT_OK) {
-
-            switch (requestCode) {
-
-                case GALLERY_CODE:
-                    sendPicture(data.getData()); //갤러리에서 가져오기
-                    break;
-                case CAMERA_CODE:
-                    getPictureForPhoto(); //카메라에서 가져오기
-                    break;
-
-                default:
-                    break;
+        if (requestCode == GET_GALLERY_IMAGE) {
+            mImageCaptureUri = data.getData();
+            final String imagePath = getRealPathFromURI(mImageCaptureUri);
+            Log.e("img Uri", imagePath);
+            // Make sure the request was successful
+            if (resultCode == RESULT_OK) {
+                try {
+                    // 선택한 이미지에서 비트맵 생성
+                    InputStream in = getContentResolver().openInputStream(data.getData());
+                    Bitmap img = BitmapFactory.decodeStream(in);
+                    in.close();
+                    // 이미지 표시
+                    ivImage.setImageBitmap(img);
+                } catch (Exception e) {
+                    e.printStackTrace();
+                }
             }
 
+            final File file = new File(imagePath);
+            RequestBody requestFile = RequestBody.create(MediaType.parse("multipart/form-data"), file);
+            final MultipartBody.Part uploadFile = MultipartBody.Part.createFormData("file", file.getName(), requestFile);
+            final RequestBody fileName = RequestBody.create(MediaType.parse("text/plain"), file.getName());
+
+
+            Call<ResponseFormat> call2 = network.uploadImage(uploadFile);
+            call2.enqueue(new Callback<ResponseFormat>() {
+                @Override
+                public void onResponse(Response<ResponseFormat> response, Retrofit retrofit) {
+                    Log.e("response", response.body().getData().toString());
+                }
+
+                @Override
+                public void onFailure(Throwable t) {
+                    Log.e("response", t.getMessage());
+                }
+            });
+
+
+            btnSend.setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View v) {
+                    String writer = textWriter.getText().toString();
+                    String title = texttitle.getText().toString();
+
+                    BoardModel bm = new BoardModel();
+                    bm.setTitle(title);
+                    bm.setFile(file.getName());
+                    bm.setWriter(writer);
+
+                    Call<ResponseFormat> call = network.post(bm);
+                    call.enqueue(new Callback<ResponseFormat>() {
+                        @Override
+                        public void onResponse(Response<ResponseFormat> response, Retrofit retrofit) {
+                            Log.e("result", response.body().toString());
+
+                            Intent intent = new Intent(BoardAddActivity.this, BoardListActivity.class);
+                            startActivity(intent);
+                        }
+
+                        @Override
+                        public void onFailure(Throwable t) {
+                            Log.e("error", t.getMessage());
+                        }
+                    });
+
+
+                }
+            });
+        }
+
+    }
+
+    private void storeCropImage(Bitmap bitmap, String filePath) {
+        String dirPath = Environment.getExternalStorageDirectory().getAbsolutePath() + "/SmartWheel";
+        File directory_SmartWheel = new File(dirPath);
+
+        if (!directory_SmartWheel.exists()) {
+            directory_SmartWheel.mkdir();
+        }
+        File copyFile = new File(filePath);
+        BufferedOutputStream out = null;
+
+        try {
+            copyFile.createNewFile();
+            out = new BufferedOutputStream(new FileOutputStream(copyFile));
+            bitmap.compress(Bitmap.CompressFormat.JPEG, 100, out);
+
+            sendBroadcast(new Intent(Intent.ACTION_MEDIA_SCANNER_SCAN_FILE, Uri.fromFile(copyFile)));
+
+            out.flush();
+            out.close();
+
+        } catch (Exception e) {
+            e.printStackTrace();
         }
 
     }
 
     private void sendPicture(Uri imgUri) {
+        String imagePath = getRealPathFromURI(imgUri);
+        Log.e("img Uri", imagePath);
 
-        String imagePath = getRealPathFromURI(imgUri); // path 경로
-        ExifInterface exif = null;
-        try {
-            exif = new ExifInterface(imagePath);
-        } catch (IOException e) {
-            e.printStackTrace();
-        }
-        int exifOrientation = exif.getAttributeInt(ExifInterface.TAG_ORIENTATION, ExifInterface.ORIENTATION_NORMAL);
-        int exifDegree = exifOrientationToDegrees(exifOrientation);
-
-        Bitmap bitmap = BitmapFactory.decodeFile(imagePath);//경로를 통해 비트맵으로 전환
-        ivImage.setImageBitmap(rotate(bitmap, exifDegree));//이미지 뷰에 비트맵 넣기
-
+        File file = new File(imagePath);
     }
 
     private String getRealPathFromURI(Uri contentUri) {
-        int column_index = 0;
         String[] proj = {MediaStore.Images.Media.DATA};
         Cursor cursor = getContentResolver().query(contentUri, proj, null, null, null);
-        if (cursor.moveToFirst()) {
-            column_index = cursor.getColumnIndexOrThrow(MediaStore.Images.Media.DATA);
-        }
+        cursor.moveToFirst();
+        int column_index = cursor.getColumnIndexOrThrow(MediaStore.Images.Media.DATA);
 
         return cursor.getString(column_index);
     }
+
 }
